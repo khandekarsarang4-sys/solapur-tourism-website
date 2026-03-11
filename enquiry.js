c3d7b6a299615dd671dd9205f192d74474096995
khandekarsarang4-sys <khandekarsarang4@gmail.com>
2026-03-11 12:00:11 +0530

    Update enquiry.js

--- enquiry.js
+++ enquiry.js
@@ -1,3 +1,20 @@
 function enquiry(){
 alert("Welcome to Solapur Tourism Website");
 }
+let images = [
+"images/pandharpur1.jpg",
+"images/pandharpur2.jpg",
+"images/siddeshwar.jpg"
+];
+
+let i = 0;
+
+function slider(){
+document.getElementById("slide").src = images[i];
+i++;
+if(i == images.length){
+i = 0;
+}
+}
+
+setInterval(slider,2000);
